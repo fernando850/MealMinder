19f89cbee6bf52adb03e4da93a2ba24d720d3899
fernando850 <f.a.martinsfernando@gmail.com>
2024-09-16 17:10:40 -0700

    console.log product nutritional information from its bar code.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const axios = require('axios');  // Import axios
 
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
@@ -12,3 +13,20 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+const getProductData = async (barcode) => {
+  try {
+    const response = await axios.get(`https://world.openfoodfacts.org/api/v2/product/${barcode}.json`);
+    const product = response.data.product;
+
+    console.log(`Product Name: ${product.product_name}`);
+    console.log(`Nutritional Info:`, product.nutriments);
+
+  } catch (error) {
+    console.error('Error fetching product data:', error);
+  }
+};
+
+// Test with a sample barcode
+getProductData('737628064502');
+
